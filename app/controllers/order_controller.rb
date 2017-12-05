5130bcc5b0553b13ac589c404c87a5dd65531806
nrednav <vr00098@surrey.ac.uk>
2017-12-05 01:45:00 +0000

    1) Refactored entire view of chinese menu with a more structured form. 2) Successfully implemented passing of quantity and selected food items id to order controller. 3) Successfully saved food items into order via habtm associations to support foods belonging to multiple orders. 4) Created a dummy template to display order information. 5) Retraced number of unique food items in the order so I find the quantities ordered and calculate the total cost of order.

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -3,12 +3,50 @@ class OrderController < ApplicationController
 	# This method assists in the creation of an order object
 	def create
 		@order = Order.new
+		@order.total = 0	
 
-		redirect_to order_path
+		# Remove items where no quantity is selected
+		@selected_foods = params[:food].select {|key, value|
+			value["quantity"] != ""
+		}
+
+		if @selected_foods.keys.empty?
+			redirect_to menu_cn_path
+		else
+			# Calculate total of order
+			@selected_foods.each do |key, value|
+				@order.total += (value["quantity"].to_i * Food.find(key).price)
+			end
+
+			# Add foods to order
+			@selected_foods.each do |key, value|
+			
+				value["quantity"].to_i.times do
+					@order.foods << Food.find(key)
+				end
+			end
+
+			@order.contact_number = "N/A"
+			@order.delivery_address = "Somewhere"
+			@order.save
+
+			redirect_to order_path
+		end
+
 	end
 
 	def show
-		@orders = Order.all
+		@order = Order.last # Get most recent order
+
+		@unique_foods = @order.foods.uniq # Retrieve a list of all the unique food items
+
+		# Retrace quantities of each item in order
+		@quantities = Hash.new(0)
+
+		@order.foods.each do |food|
+			@quantities[food] += 1
+		end
+
 	end
 
 end
